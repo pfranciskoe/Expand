338cb00fd9a02ecf660b281c482918109da05f5a
peter <pfranciskoe@gmail.com>
2020-05-12 13:59:08 -0700

    working seed and routes

--- seed.js
+++ seed.js
@@ -4,34 +4,6 @@ const Course = require('./models/Course.js')
 const mongoose = require('mongoose')
 mongoose.connect(mongoURL.mongoURI, {useNewUrlParser: true});
 
-// const seeder = require('mongoose-seed');
-// // Connect to MongoDB via Mongoose
-// seeder.connect(address.mongoURI, function () {
-
-//     // Load Mongoose models
-//     seeder.loadModels([
-//         './models/Course.js',
-//         './models/User.js'
-//     ]);
-
-//     // Clear specified collections
-//     seeder.clearModels(['Course', 'User'], function () {
-
-//         // Callback to populate DB once collections have been cleared
-//         seeder.populateModels(data, function () {
-//             seeder.disconnect();
-//         });
-
-//     });
-// });
-
-// // Data array containing seed data - documents organized by Model
-
-
-// const data = [
-//     {
-//         'model': 'User',
-//         'documents': [
 User.deleteMany({},(err)=>console.log(err))
 Course.deleteMany({},(err)=>console.log(err))
 
@@ -65,19 +37,19 @@ User.create(users_arr, function (err, users) {
         if (err) 
                 {console.log(err)}
         else {
-                let user1 = users[0]._id;
-                let user2 = users[1]._id;
-                let user3 = users[2]._id;
+                let user1 = users[0];
+                let user2 = users[1];
+                let user3 = users[2];
                 const courses_arr = [
                     {
-                            'instructor': `${user1}`,
-                            'students': [`${user2}`, `${user3}`],
+                            'instructor': `${user1._id}`,
+                            'students': [ `${user3._id}`],
                             'title': 'Potato Farming',
                             'description': 'Self explanatory',
                             'lessons': []
                     }, {
-                            'instructor': `${user1}`,
-                            'students': [`${user2}`, `${user3}`],
+                            'instructor': `${user1._id}`,
+                            'students': [`${user2._id}`, `${user3._id}`],
                             'title': 'Box Crushing',
                             'description': 'Less violent than it sounds',
                             'lessons': [],
@@ -89,7 +61,12 @@ User.create(users_arr, function (err, users) {
                             
                             let course0 = courses[0]._id;
                             let course1 = courses[1]._id;
-                            user2.courses = [course0, course1]
+                            user1.courses = [course0, course1]
+                            user1.save();
+                            user2.courses = [ course1]
+                            user2.save();
+                            user3.courses = [course0, course1]
+                            user3.save();
                     }
                 
                 })
@@ -101,24 +78,3 @@ User.create(users_arr, function (err, users) {
 
 
 
-
-// const course1 = Course.create(
-//     {
-//         'instructor': `${user1._id}`,
-//         'students': [`${user2._id}`, `${user3._id}`],
-//         'title': 'Potato Farming',
-//         'description': 'Self explanatory',
-//         'lessons': []
-//     },
-// )
-
-// const course2 = Course.create(
-//     {
-        
-//         'instructor': `${user1._id}`,
-//         'students': [`${user2._id}`, `${user3._id}`],
-//         'title': 'Box Crushing',
-//         'description': 'Less violent than it sounds',
-//         'lessons': [],
-//     },
-// )
