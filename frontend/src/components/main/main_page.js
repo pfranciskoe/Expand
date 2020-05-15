b6527ad824771ffd2f17d62ad25e863abc9f354c
zlin <liz.zxlin@gmail.com>
2020-05-15 10:20:36 -0700

    add styling to splash page

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import "../../stylesheets/main.css";
+import {Link} from "react-router-dom"
 
 class MainPage extends React.Component {
 
     render() {
         const splashStyle = {
           backgroundImage:
-            "url(https://images.unsplash.com/photo-1522071901873-411886a10004?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80)",
+            "url(https://expand-dev.s3-us-west-1.amazonaws.com/images/table1.png)",
         }; 
         const bannerStyle = {
-          backgroundImage: 'url(https://expand-dev.s3-us-west-1.amazonaws.com/images/resize2.png)'
+          backgroundImage:
+            "url(https://expand-dev.s3-us-west-1.amazonaws.com/images/banner-dots.png)",
         };
         return (
           <div className="main-box">
@@ -17,6 +19,7 @@ class MainPage extends React.Component {
               <div className="overlay">
                 <div className="splash-content">
                   <h1>Expand your online learning experience.</h1>
+                  <Link to="/signup">Start Today</Link>
                 </div>
               </div>
             </div>
