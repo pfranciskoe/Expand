09ea0729160f113e1fa459509ab2d9a5770160fd
zlin <liz.zxlin@gmail.com>
2020-05-14 17:23:28 -0700

    adding header in splash

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -5,7 +5,8 @@ class MainPage extends React.Component {
 
     render() {
         const splashStyle = {
-          backgroundImage: 'url(https://expand-dev.s3-us-west-1.amazonaws.com/images/expand-main-splash.jpg)'
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1522071901873-411886a10004?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80)",
         }; 
         const bannerStyle = {
           backgroundImage: 'url(https://expand-dev.s3-us-west-1.amazonaws.com/images/resize2.png)'
@@ -19,6 +20,7 @@ class MainPage extends React.Component {
                 </div>
               </div>
             </div>
+            <h1 className="banner-header"> What Make Us Unique</h1>
             <div className="banner-box" style={bannerStyle}></div>
             <footer></footer>
           </div>
