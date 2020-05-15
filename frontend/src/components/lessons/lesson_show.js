8ba73e3a75bd72d5bddc9ccc81de059450f59265
peter <pfranciskoe@gmail.com>
2020-05-15 11:09:40 -0700

    fixed styling

--- frontend/src/components/lessons/lesson_show.js
+++ frontend/src/components/lessons/lesson_show.js
@@ -1,28 +1,29 @@
-import React from "react";
-import "../../stylesheets/lesson.css"
+import React from 'react'
+import '../../stylesheets/lesson.css'
 import Comment from './comments/comment'
 import CommentHeatmap from './comments/comment_heatmap'
 
-class LessonShow extends React.Component{
-    constructor(props){
-        super(props)
-        this.state={loading:true, loading2:true, text:'', form:false}
-        this.handleSubmit=this.handleSubmit.bind(this)
-        this.handleChange=this.handleChange.bind(this)
-        this.handleScroll=this.handleScroll.bind(this)
-    }
+class LessonShow extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { loading: true, loading2: true, text: '', form: false }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleScroll = this.handleScroll.bind(this)
+  }
 
-    componentDidMount(){
-        this.props.getLesson(this.props.match.params.id).then(()=>this.setState({loading:false})).then(()=>{
-        const vid = document.getElementsByClassName('video-tag')[0]
-          vid.onloadedmetadata = () => {
-          this.vid = vid
-          this.vidLength = Math.floor(vid.duration)
-          this.setState({ loading2: false})
-        }
-        })
-    }
-  handleSubmit(event) {
+  componentDidMount () {
+    this.props.getLesson(this.props.match.params.id).then(() => this.setState({ loading: false })).then(() => {
+      const vid = document.getElementsByClassName('video-tag')[0]
+      vid.onloadedmetadata = () => {
+        this.vid = vid
+        this.vidLength = Math.floor(vid.duration)
+        this.setState({ loading2: false })
+      }
+    })
+  }
+
+  handleSubmit (event) {
     event.preventDefault()
     if (this.state.text) {
       const resp = {
@@ -32,67 +33,71 @@ class LessonShow extends React.Component{
         timestamp: this.vid.currentTime
       }
       this.props.createComment(resp)
-        .then(() => this.setState({ form: false, loading2: true, text: ''}))
+        .then(() => this.setState({ form: false, loading2: true, text: '' }))
         .then(
-          ()=>this.props.getLesson(this.props.match.params.id)
-      ).then(() => this.setState({ loading2: false, text: ''}))
+          () => this.props.getLesson(this.props.match.params.id)
+        ).then(() => this.setState({ loading2: false, text: '' }))
     } else {
       this.setState({ form: false })
     }
   }
-  handleChange(event) {
+
+  handleChange (event) {
     this.setState({ text: event.target.value })
   }
-  handleScroll(){
+
+  handleScroll () {
     const ele = document.getElementById('comment-box')
     const bodyRect = document.body.getBoundingClientRect()
     const elemRect = ele.getBoundingClientRect()
-    const offset = elemRect.top - bodyRect.top;
-    window.scrollTo(0,offset-80)
+    const offset = elemRect.top - bodyRect.top
+    window.scrollTo(0, offset - 80)
   }
-    render(){
-      if (this.state.loading === true) {
-        return(
-          <div></div> 
-        )
-      } else {
-        return (
-          <div  className='lesson-show-page'>
-            <video className='video-tag' controls>
-              <source src={this.props.lesson.videoUrl}
-              type="video/mp4" />
-            </video>
-            {this.state.loading2 == false ?
-              <CommentHeatmap onHit={this.handleScroll} lesson={this.props.lesson} vidLength={this.vidLength}/> : null }
-            <div className='lesson-info'>
-              <p className='lesson-info-title'>{this.props.lesson.title}</p>
-              <p className='lesson-info-instructor'>
-                {this.props.lesson.instructor.firstName} {this.props.lesson.instructor.lastName}
-                </p>
-              <p className='lesson-info-desc'>{this.props.lesson.description}</p>
-              <div className='comment-buttons'>
+
+  render () {
+    if (this.state.loading === true) {
+      return (
+        <div />
+      )
+    } else {
+      return (
+        <div className='lesson-show-page'>
+          <video className='video-tag' controls>
+            <source
+              src={this.props.lesson.videoUrl}
+              type='video/mp4'
+            />
+          </video>
+          {this.state.loading2 == false
+            ? <CommentHeatmap onHit={this.handleScroll} lesson={this.props.lesson} vidLength={this.vidLength} /> : null}
+          <div className='lesson-info'>
+            <p className='lesson-info-title'>{this.props.lesson.title}</p>
+            <p className='lesson-info-instructor'>
+              {this.props.lesson.instructor.firstName} {this.props.lesson.instructor.lastName}
+            </p>
+            <p className='lesson-info-desc'>{this.props.lesson.description}</p>
+            <div className='comment-buttons'>
               {this.state.form
-                ?
-                <form>
+                ? <form>
                   <textarea className='comment-input' onChange={this.handleChange} value={this.state.text} />
                   <button className='comment-button' onClick={this.handleSubmit}>Submit</button>
-                </form>
-                :
-                <button className='comment-button' onClick={() => this.setState({ form: true })}>Comment</button>
-              }
-              </div>
-            </div>
-            <div className='comment-box' id='comment-box'>
-              {Object.values(this.props.lesson.comments).map((comment,idx)=>(
-                <Comment key={`comment-${idx}`} comment={comment} createResponse={this.props.createResponse}
-                  currentUserId={this.props.currentUserId} getLesson={() => this.props.getLesson(this.props.match.params.id)}
-                  deleteComment={() => this.props.deleteComment(comment._id)} deleteResponse={this.props.deleteResponse}/>
-              ))}
+                  </form>
+                : <button className='comment-button' onClick={() => this.setState({ form: true })}>Comment</button>}
             </div>
           </div>
-        )
-      }
+          <div className='comment-box' id='comment-box'>
+            {Object.values(this.props.lesson.comments).map((comment, idx) => (
+              <Comment
+                key={`comment-${idx}`} comment={comment} createResponse={this.props.createResponse}
+                currentUserId={this.props.currentUserId} getLesson={() => this.props.getLesson(this.props.match.params.id)}
+                deleteComment={() => this.props.deleteComment(comment._id)} deleteResponse={this.props.deleteResponse}
+              />
+            ))}
+          </div>
+        </div>
+      )
     }
+  }
 }
 
-export default LessonShow;
+export default LessonShow
