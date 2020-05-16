ae26f5bd1137dc19b8c896ed518d877edb5f4bf6
Erick Santos <eksantos.jr@gmail.com>
2020-05-16 14:18:26 -0700

    Add lesson errors slice of state

--- frontend/src/reducers/errors_reducer.js
+++ frontend/src/reducers/errors_reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import SessionErrorsReducer from './session_errors_reducer';
 import CoursesErrorsReducer from './courses_errors_reducer';
 import UsersErrorsReducer from './users_errors_reducer';
+import LessonsErrorsReducer from './lessons_errors_reducer';
 
 export default combineReducers({
     users: UsersErrorsReducer,
     courses: CoursesErrorsReducer,
+    lessons: LessonsErrorsReducer,
     session: SessionErrorsReducer
 });
---
+++ frontend/src/reducers/lessons_errors_reducer.js
@@ -0,0 +1,11 @@
+import {RECEIVE_LESSON_ERRORS} from '../actions/lessons_actions';
+
+export default (state = [], action) => {
+    Object.freeze(state);
+    switch(action.type){
+        case RECEIVE_LESSON_ERRORS:
+            return action.errors;
+        default:
+            return state;
+    }
+}
