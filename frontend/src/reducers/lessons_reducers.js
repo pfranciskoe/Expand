6ad7ae8abbfdc4370012da3f7eefcbc2bac9c83d
Erick Santos <eksantos.jr@gmail.com>
2020-06-03 16:18:00 -0700

    Commented out axios req to delete lesson

--- frontend/src/reducers/lessons_reducers.js
+++ frontend/src/reducers/lessons_reducers.js
@@ -1,9 +1,7 @@
 import {
-  RECEIVE_LESSONS,
   RECEIVE_LESSON,
   REMOVE_LESSON,
 } from "../actions/lessons_actions";
-import { RECEIVE_COURSE } from "../actions/courses_actions";
 
 export default (state = {}, action) => {
   Object.freeze(state);
@@ -11,8 +9,6 @@ export default (state = {}, action) => {
     case RECEIVE_LESSON:
         return {...state, [action.lesson.data._id]: action.lesson.data}
     case REMOVE_LESSON:
-      console.log("reducer deletion", action.lessonId)
-      console.log("actual: ", action.lessonId.data._id)
         const newState = {...state}
         delete newState[action.lessonId.data._id];
         return newState;
